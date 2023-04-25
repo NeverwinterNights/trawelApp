16523a19bae2d6c1f378150aa9df94e7d966f082
Pavel Kardash <cardash@mail.ru>
2023-04-25 23:16:43 +0300

    done intro

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {MainNavigator} from './src/navigations/MainNavigator';
 import {NavigationContainer} from '@react-navigation/native';
+import RNBootSplash from 'react-native-bootsplash';
 
 const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide();
+  }, []);
+
   return (
     <NavigationContainer>
       <MainNavigator />
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,54 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {
+  Image,
+  SafeAreaView,
+  StatusBar,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 type HomePropsType = {};
 
 export const Home = ({}: HomePropsType) => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
+    <SafeAreaView className="bg-white flex-1 relative">
+      <StatusBar backgroundColor={'white'} barStyle={'dark-content'} />
+      {/*  Header*/}
+      <View className="flex-row mt-8 px-4 items-center space-x-2">
+        <View className="w-16 h-16 bg-black rounded-full items-center justify-center">
+          <Text className="text-[#00BCC9] font-semibold text-3xl">Go</Text>
+        </View>
+        <Text className="text-[#2A2B4B] font-semibold text-3xl">Travel</Text>
+      </View>
+      {/*  Second*/}
+      <View className="mt-8 px-4 space-y-3">
+        <Text className="text-[#3C6072] text-[42px]">Enjoy the trip with</Text>
+        <Text className="text-[#00BCC9] text-[38px] font-bold">
+          Good Moments
+        </Text>
+        <Text className="text-[#3C6072] text-base">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Animi
+          dignissimos eligendi eum facere
+        </Text>
+      </View>
+      {/*  Circles*/}
+      <View className="w-[400px] h-[400px] bg-[#00BCC9] rounded-full absolute bottom-[90px] -right-36" />
+      <View className="w-[400px] h-[400px] bg-[#E99265] rounded-full absolute -bottom-28 -left-36" />
+      {/*  Image*/}
+      <View className="flex-1 relative items-center justify-center">
+        <Image
+          source={require('../../assets/hero.png')}
+          className="w-full h-full"
+        />
+        <View className="absolute bottom-20 w-24 h-24 border-[#00BCC9] border-t-4 border-r-4 border-l-2 rounded-full items-center justify-center">
+          <TouchableOpacity>
+            <View className=" items-center justify-center w-20 h-20 rounded-full bg-[#00BCC9]">
+              <Text className="text-gray-50 text-[40px] font-semibold">Go</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
